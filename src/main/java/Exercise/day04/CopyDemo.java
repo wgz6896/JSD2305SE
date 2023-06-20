9d24d9850afeb61b2495897fbf08ce27268c436d
wgz <2111377105@qq.com>
2023-06-20 19:15:35 +0800

    JavaSE

--- src/main/java/Exercise/day04/CopyDemo.java
+++ src/main/java/Exercise/day04/CopyDemo.java
@@ -15,16 +15,22 @@ public class CopyDemo {
          * 先对文件进行读操作，然后对一个新的文件进行写操作，那就是需要IOStream
          */
         FileInputStream fis  =new FileInputStream("1.jpg");
-        FileOutputStream fos = new FileOutputStream("02.jpg");
-
+        FileOutputStream fos = new FileOutputStream("3.jpg");
+        byte[]data = new byte[888];
+        long start = System.currentTimeMillis();
+        int len;
+        while ((len= fis.read())!=-1){
+        fos.write(len);}
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
         //首先进行一个读数据,但是这样只能一个一个读，效率慢而且需要考虑全部字节，应该思考循环
         //也就是需要有一个循环条件，read读到末尾回返回一个-1，也就是判断这个read == -1
 //        int d ;
 //        while((d=fis.read())!=-1){
 //            fos.write(d);
 //        }
-//        fis.close();
-//        fos.close();
+        fis.close();
+        fos.close();
 
     }
 }
